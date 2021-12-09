96aac273583105b91340e67412659635dc836f0d
Wenxu Li <wenxu@Juliens-iMac.fritz.box>
2021-12-09 18:00:12 +0100

    subspace clustering

--- subspace_clustering.py
+++ subspace_clustering.py
@@ -4,15 +4,17 @@ from sklearn.cluster import SpectralCoclustering
 from sklearn.cluster import SpectralBiclustering
 from sklearn.metrics import consensus_score
 import pandas as pd
+from sklearn.datasets import make_checkerboard
 
 
 def prepare_data(path):
-    df = pd.read_csv(path, sep=',').fillna(0)
-    df = df.replace([np.inf, -np.inf], 0)
+    df = pd.read_csv(path, sep=',')
     X = df.to_numpy()
+    print(type(X))
+    X = np.nan_to_num(X)
     plt.matshow(X, cmap=plt.cm.Blues, interpolation="none")
     plt.title("Original dataset")
-    # plt.show()
+    plt.show()
     return X
 
 
@@ -28,11 +30,19 @@ def subspace_clustering(n_cluster, X, name):
 
     plt.matshow(np.outer(np.sort(model.row_labels_) + 1, np.sort(model.column_labels_) + 1),
                 cmap=plt.cm.Blues)
+
     plt.title("Checkerboard structure of rearranged data")
-    # plt.show()
+    plt.show()
+    rng = np.random.RandomState(0)
+    row_idx = rng.permutation(X.shape[0])
+    col_idx = rng.permutation(X.shape[1])
+
+    data, rows, columns = make_checkerboard(
+        shape=(X.shape[0], X.shape[1]), n_clusters=n_cluster, random_state=0)
+    score = consensus_score(model.biclusters_, (rows[:, row_idx], columns[:, col_idx]))
+    print("consensus score: {:.1f}".format(score))
 
 
 if __name__ == '__main__':
     X = prepare_data("/Users/wenxu/PycharmProjects/DataChallenge/data/task_3/dr_age_sub_20_40.csv")
-    print(X)
     subspace_clustering(3, X, 'SpectralCoclustering')
