a724ea50b5a5d410b2d75c96a062e3bcaa230baa
Wenxu Li <wenxu@vpn-ssl-233-086.rz.uni-frankfurt.de>
2021-12-20 22:33:32 +0100

    split task 3 from dr

--- dim_reduction.py
+++ dim_reduction.py
@@ -15,13 +15,13 @@ def dr(path):
     X = df[df.columns[:-1]].to_numpy()
     y = df['SepsisLabel'].to_numpy()
     ###### only for task 3
-    SepsisLabel = df['SepsisLabel']
+    # SepsisLabel = df['SepsisLabel']
     ######
 
     algorithms = {
-        # 't-SNE': TSNE(),
-        # 'UMAP': umap.UMAP(),
-        # 'TriMAP': trimap.TRIMAP(),
+        't-SNE': TSNE(),
+        'UMAP': umap.UMAP(),
+        'TriMAP': trimap.TRIMAP(),
         'PaCMAP': pacmap.PaCMAP(n_dims=2, n_neighbors=None, MN_ratio=0.5, FP_ratio=2.0)
     }
 
@@ -33,8 +33,8 @@ def dr(path):
         if name == "PaCMAP":
             embedding = algorithm.fit_transform(X, init='pca')
             ##### for task 3 generate dim reduced csv
-            df = pd.DataFrame(embedding, columns=['x', 'y'])
-            df = pd.concat([df, SepsisLabel], axis=1)
+            # df = pd.DataFrame(embedding, columns=['x', 'y'])
+            # df = pd.concat([df, SepsisLabel], axis=1)
             #####
             embeddings[name] = pd.DataFrame(embedding, columns=['x', 'y'])
             durations[name] = time.time() - t0
@@ -43,24 +43,23 @@ def dr(path):
             embeddings[name] = pd.DataFrame(embedding, columns=['x', 'y'])
             durations[name] = time.time() - t0
     ###### only for task 3
-    df.to_csv('dr_age_sub_20_40.csv', index=False)
+    # df.to_csv('dr_age_sub_20_40.csv', index=False)
     #######
 
 
-    # fig, ax = plt.subplots(4, figsize=(6, 6))
-    # for i, j in enumerate(embeddings):
-    #     ax[i].scatter(embeddings[j]['x'], embeddings[j]['y'], cmap="Spectral", c=y, s=0.6)
-    #     ax[i].set_title(f"Dimentionality reduced by {j}")
-    # fig.tight_layout()
-    # # st.pyplot(fig)
-    # plt.show()
+    fig, ax = plt.subplots(4, figsize=(6, 6))
+    for i, j in enumerate(embeddings):
+        ax[i].scatter(embeddings[j]['x'], embeddings[j]['y'], cmap="YlOrBr_r", c=y, s=0.6)
+        ax[i].set_title(f"Dimentionality reduced by {j}")
+    fig.tight_layout()
+    # st.pyplot(fig)
+    plt.show()
 
 
 if __name__ == '__main__':
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/gender/Gender0")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/gender/Gender1")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputation/mean.csv")
-    dr(path="//data/age/20<Age<40.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/age/40<Age<70.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/age/Age>70.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputation/meansetA.csv")
@@ -71,6 +70,6 @@ if __name__ == '__main__':
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/20<linear<40.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/40<linear<70.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/70<linear.csv")
-    # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/20<mean<40.csv")
+    dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/20<mean<40.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/40<mean<70.csv")
     # dr(path="/Users/wenxu/PycharmProjects/DataChallenge/data/imputed_subgroup/70<mean.csv")
